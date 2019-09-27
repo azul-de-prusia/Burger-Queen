054d18e343fc1a700fdbe954da2df992042246ba
azul-de-prusia <tegthyn_600@hotmail.com>
2019-09-27 16:50:48 -0500

    first components

---
+++ src/Components/Kitchenconfirm.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class KitchenConfirm extends Component {
+    constructor() {
+        super();
+    }
+render() {
+    return(
+    <div>
+
+    }
+}
+
+export default KitchenConfirm;
---
+++ src/Components/WaitForFood.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class WaitForFood extends Component {
+    constructor() {
+        super();
+    }
+render() {
+    return(
+    <div>
+
+    }
+}
+
+export default WaitForFood;
