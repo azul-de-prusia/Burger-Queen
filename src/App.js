cfa17f455775e02c8523e81281b91463f9d5f40e
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-10-03 15:15:18 -0500

    dessing and style

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ function App() {
     <div className="App">
     
       <header className="App-header">
-      <h1>Interstellar</h1>
+      {<img className="interrestellar" src="https://i.ibb.co/khfm8TM/ezgif-com-crop-5.gif"></img>}
+    
       </header>
-     
+      {<img className="space" src="https://i.gifer.com/58RA.gif"></img>}
       <Routes />
      
     </div>
