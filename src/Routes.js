c8edc1c495f5f37daeebe9168e6e6948375808dc
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-10-07 14:46:43 -0500

    se modificaron las rutas

--- src/Routes.js
+++ src/Routes.js
@@ -7,10 +7,7 @@ import OrderReady from './Components/OrderReady';
 import SendOrder from './Components/Sendorder';
 import WaitForFood from './Components/WaitForFood';
 import Home from './Components/Home';
-import App2 from './Components/App2';
-import Title from './Components2/Title';
-import RecipeCard from './Components2/RecipeCard';
-import Image from './Components2/Image';
+
 
 
 
@@ -18,10 +15,6 @@ import Image from './Components2/Image';
 const Routes = () => (
     <BrowserRouter>
     <Switch>
-    <Route path="/App2" component ={App2}/>
-    <Route path="/Title" component ={Title}/>
-    <Route path="/Image" component ={Image}/>
-    <Route path="/RecipeCard" component ={RecipeCard}/>
       <Route path="/AlertYourFoodIsReady" component ={AlertYourFoodIsReady}/>
       <Route path="/KitchenConfirm" component ={KitchenConfirm}/>
       <Route path="/Menu" component ={Menu}/>
