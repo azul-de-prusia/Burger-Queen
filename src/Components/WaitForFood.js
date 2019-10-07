4c686facfc6ec229607fc859ea60551439b3383e
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-10-07 16:40:12 -0500

    actualiacion de enu y ventana de wait for food lista

--- src/Components/WaitForFood.js
+++ src/Components/WaitForFood.js
@@ -7,8 +7,9 @@ class WaitForFood extends Component {
 render() {
     return(
     <div>
+         {<img src="https://66.media.tumblr.com/15fb32db6e1dd51f4ad43319a30de494/tumblr_n6pqj9Q6qB1sqsanfo1_400.gif"></img>}
         <p>Your order has been send, it will be ready about 30 minutes</p>
-       <button>cancel</button>
+          <button className="cancel" >cancel</button>
         <p>After 6 minutes you cannot cancel</p>
     </div>
     )
