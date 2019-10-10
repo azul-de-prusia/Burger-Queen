b8fd4752f9ba4e61dcb2be7364b71cceab557922
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-10-10 17:08:20 -0500

    se modificaron los cambios de pantalla

--- src/Components/Kitchenconfirm.js
+++ src/Components/Kitchenconfirm.js
@@ -7,6 +7,10 @@ class KitchenConfirm extends Component {
 render() {
     return(
     <div>
+<h1>KITCHEN STAFF</h1>
+<a href="/WaitForFood">
+     <button className="buttonsubmit"></button>
+     </a>
     </div>
     )    
 }
--- src/Components/WaitForFood.js
+++ src/Components/WaitForFood.js
@@ -6,27 +6,13 @@ class WaitForFood extends Component {
     }
 render() {
     return(
-
-    <div className="">
-         {<img className="spaceship"src="https://66.media.tumblr.com/15fb32db6e1dd51f4ad43319a30de494/tumblr_n6pqj9Q6qB1sqsanfo1_400.gif"></img>}
-        <p>Your order has been send, </p>
-        <p>it will be ready about </p>
-        {<img  src="https://i.ibb.co/SRVLJGF/ezgif-com-crop-6.gif"></img>}
-        <p>minutes</p>          
-
-        <div>
-    <a href="/OrderReady">
+    <div>
+         {<img src="https://66.media.tumblr.com/15fb32db6e1dd51f4ad43319a30de494/tumblr_n6pqj9Q6qB1sqsanfo1_400.gif"></img>}
+        <p>Your order has been send,</p> 
+        <p>it will be ready about</p>
+    <a href="/AlertYourFoodIsReady">
      <button className="buttonFood"></button>
      </a>
-    </div>
-          
-        <a href="/App2">
-        <p>You want cancel?</p>
-     
-     </a>
-          
-    
-        <p>After 6 minutes you cannot cancel</p>
     </div>
     )
     }
