0e673d391832ec2b59bb791301b99323924ca47f
azul-de-prusia <tegthyn_600@hotmail.com>
2019-10-14 15:19:33 -0500

    Updates

---
+++ precache-manifest.abef1f6130628a046d818548f2e49df1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989826679999299179684b61946f0ec2",
+    "url": "/Burger-Queen/index.html"
+  },
+  {
+    "revision": "9eb9c5fc7b9a2ce789a4",
+    "url": "/Burger-Queen/static/css/main.16ca709a.chunk.css"
+  },
+  {
+    "revision": "fe576605026f671f5391",
+    "url": "/Burger-Queen/static/js/2.66bb5c8c.chunk.js"
+  },
+  {
+    "revision": "9eb9c5fc7b9a2ce789a4",
+    "url": "/Burger-Queen/static/js/main.ed25ccc1.chunk.js"
+  },
+  {
+    "revision": "36ed12848a22a55121ad",
+    "url": "/Burger-Queen/static/js/runtime-main.db180956.js"
+  },
+  {
+    "revision": "4f931fb474a7ab3eeb414bf77d5dced2",
+    "url": "/Burger-Queen/static/media/coca.4f931fb4.jpg"
+  },
+  {
+    "revision": "7ff2e5e39f7db2842403dcc78a72b13d",
+    "url": "/Burger-Queen/static/media/hambuerger.7ff2e5e3.jfif"
+  },
+  {
+    "revision": "7dc9f1719044740ad0fc95b4b8fa94cb",
+    "url": "/Burger-Queen/static/media/pizzza.7dc9f171.jpg"
+  }
+]);
